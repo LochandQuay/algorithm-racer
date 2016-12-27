09fccc5d48c3a5bfadba5d9cd6e2f51e04185646
gettinToasty <sean.r.beyer@gmail.com>
2016-12-26 19:56:33 -0500

    Add user authentication

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,20 @@
+class UserController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+end
