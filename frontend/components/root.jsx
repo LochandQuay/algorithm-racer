5310620365680c2315832c1f3a770cef39901a54
gettinToasty <sean.r.beyer@gmail.com>
2016-12-30 17:19:13 -0500

    Fix formatting of logout button

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,8 +3,8 @@ import { Provider } from 'react-redux';
 import Editor from './editor.jsx';
 
 const Root = ({ store }) => (
-  <div> // change to store provider??
-    // <Leaderboard /> goes here
+  <div> {/* change to store provider?? */}
+    {/* <Leaderboard /> goes here */}
     <Editor />
   </div>
 );
