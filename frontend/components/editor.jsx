76c156f7fd0607a0027eb0467498c8765e0fe241
gettinToasty <sean.r.beyer@gmail.com>
2017-01-01 20:47:39 -0800

    Change placeholder text

--- frontend/components/editor.jsx
+++ frontend/components/editor.jsx
@@ -267,6 +267,11 @@ let Editor = React.createClass({
 			readOnly: this.state.readOnly,
 			mode: this.state.mode
 		};
+
+		let placeholder = () => {
+			let text = (this.state.category === "SORT") ? "[11, 5, 3, 7]" : "([11, 5, 3, 7], 3)"
+			return text + " // test arguments"
+		}
 		return (
 			<div>
 				<h2>Submit an Algorithm</h2>
@@ -309,7 +314,7 @@ let Editor = React.createClass({
 				<div id="editor-testing">
 				  <input onChange={this.updateTestArgs}
 							   type='text'
-							 	 placeholder={(this.state.category === "SORT") ? "[11, 5, 3, 7]" : "([11, 5, 3, 7], 3)"}
+							 	 placeholder={placeholder()}
 							   value={this.state.testArgs} />
 
 					<textarea value={this.state.testOutput}
