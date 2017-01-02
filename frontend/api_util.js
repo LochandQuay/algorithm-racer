b34cdad452ef4879736290801ab5077415f307e4
gettinToasty <sean.r.beyer@gmail.com>
2017-01-01 23:15:51 -0800

    Change URLs to heroku

--- frontend/api_util.js
+++ frontend/api_util.js
@@ -1,7 +1,7 @@
 const APIUtil = {
   createAlgo: data => $.ajax({
     type: 'POST',
-    url: 'http://localhost:3000/algorithms',
+    url: 'https://algorithm-racer.herokuapp.com/algorithms',
     dataType: 'JSON',
     contentType: 'application/json',
     data: JSON.stringify(data)
@@ -9,7 +9,7 @@ const APIUtil = {
 
   fetchScores: (category, maxScore) => $.ajax({
     method: 'GET',
-    url: 'http://localhost:3000/algorithms',
+    url: 'https://algorithm-racer.herokuapp.com/algorithms',
     dataType: 'json',
     data: {
       category: category,
